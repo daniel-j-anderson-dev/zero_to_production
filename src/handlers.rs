67aa635718d48c51fb2c741cf065a4f089dda49c
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-04-22 16:54:19 -0400

    made greet use sentence case

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,6 +1,21 @@
-use actix_web::{HttpRequest, Responder};
+use actix_web::{HttpRequest, HttpResponse, Responder};
 
 pub async fn greet(request: HttpRequest) -> impl Responder {
     let name = request.match_info().get("name").unwrap_or("World");
-    return format!("Hello {}", name);
+    return format!("Hello {}", sentence_case(name));
+}
+
+fn sentence_case(input: &str) -> String {
+    let output = input
+        .chars()
+        .enumerate()
+        .map(|(i, c)| {
+            if i == 0 {
+                c.to_uppercase()
+            } else {
+                c.to_lowercase()
+            }
+        })
+        .collect();
+    return output;
 }
