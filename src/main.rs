e7b2727f43c3fe3e3e9a0e030109ba2a9dc91eaa
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-04-22 16:57:25 -0400

    Changed routes

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,8 @@ use actix_web::{web::get, App, HttpServer};
 async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
-            .route("/", get().to(greet))
+            .route("/", get().to(health_check))
             .route("/{name}", get().to(greet))
-            .route("/{health_check}", get().to(health_check(request)))
     })
     .bind("localhost:8000")?
     .run()
